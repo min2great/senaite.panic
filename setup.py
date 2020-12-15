ad569282d5919009ac3383566c1dfc9a0d91a377
valeriozhang <valeriozhang14@gmail.com>
2020-12-15 23:15:02 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "1.0.1rc4"
+version = "1.0.1rc5"
 
 setup(
     name="valer.panic",
@@ -27,7 +27,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "valer.lims==1.3.4rc4",
+        "valer.lims==1.3.4rc5",
         "requests",
     ],
     extras_require={
